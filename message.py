df0fc3082f2ec6b6b9ec1500639565a3a6eb0662
Elon <76097179+elonadam@users.noreply.github.com>
2024-12-06 16:13:07 +0200

    message struct with validataion

--- message.py
+++ message.py
@@ -5,11 +5,10 @@ class Message:
     """
     represent a single message
     """
-
+    # check for a valid number, got an issue with DB to start the number with 0
     def __init__(self, sender_id, recipient_id, subject, content, date, blue_v):
 
         # validate numbers, will get 54 along with 054, cant check 05X because octal base
-        # I can add this line if really want to check for 05, and str(sender_id).startswith("05")
         if not (isinstance(sender_id, int) and 500000000 <= sender_id <= 599999999):
             raise ValueError("sender_id must be a positive 10-digit number starting with '05'")
         if not (isinstance(recipient_id, int) and 500000000 <= recipient_id <= 599999999):
