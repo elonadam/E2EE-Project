83eef2f70df168442dd7147e026e603f0f0bb2f1
Elon <76097179+elonadam@users.noreply.github.com>
2024-12-10 18:57:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from db_manager import DatabaseManager
 from Rulon_GUI import StartWindow
 
 db = DatabaseManager()
-
+db.create_tables()
 if __name__ == "__main__":
     start = StartWindow()
     start.mainloop()
