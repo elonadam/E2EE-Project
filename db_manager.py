cc97f59eda3cbaec0eb0cc04a129d239f77b69db
Elon <76097179+elonadam@users.noreply.github.com>
2024-12-17 18:46:23 +0200

    Update db_manager.py

--- db_manager.py
+++ db_manager.py
@@ -100,7 +100,7 @@ class DatabaseManager:
 
     def fetch_messages_for_user(self, user_phone):
 
-        self.c.execute("UPDATE messages SET blue_v = ? WHERE recipient_phone = ?", (True, user_phone))
+        self.c.execute("UPDATE messages SET blue_v = ? WHERE recipient_phone = ? AND blue_v = ?", (True, user_phone, 0))
         self.conn.commit()
         self.c.execute(
             "SELECT sender_phone, recipient_phone, encrypted_aes_key, iv, ciphertext, date, blue_v FROM messages WHERE recipient_phone=?",
