0dc74af879edc9edeb83b92db61837c2495f796a
Elon <76097179+elonadam@users.noreply.github.com>
2024-12-22 22:18:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,22 +9,3 @@ if __name__ == "__main__":
     start = StartWindow()
     start.mainloop()
 
-
-
-
-
-
-
-
-# test zone #####################################################
-
-# Add a user
-#add_user(1, "John Doe")
-
-# Add a message
-# add_message(5000, 1, 'dope', "auto increment work.")
-
-#db.create_tables()
-
-
-# end of test zone #############################################
